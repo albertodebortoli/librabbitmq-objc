1339a733a1158917f017c2e91cbfcd04d440eb47
Alberto De Bortoli <albertodebortoli@gmail.com>
2014-02-05 14:33:31 +0100

    Added building demo project, crashing due to librabbitmq-objc using an incompatible version of rabbitmq-c.

---
+++ librabbitmq-objc-demo/librabbitmq-objc-demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  librabbitmq-objc-demo
+//
+//  Created by Alberto De Bortoli on 13/01/2014.
+//  Copyright (c) 2014 EF Education First. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
